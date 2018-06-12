4ccb3f5fbac9d83db15f65e6db5c0f4fd9317a74
Andrea Grazioso <grazioandre@gmail.com>
2018-06-12 17:49:33 +0200

    Adding recon_daemon database connector
    Added Struct, info, header and main implementation for the connector
    with the existing mysql database

---
+++ recon_daemon/DBStruct.h
@@ -0,0 +1,15 @@
+#ifndef RECON_DBSTRUCT_H
+#define RECON_DBSTRUCT_H
+
+namespace DBStruct{
+
+    struct node{
+        std::string key;
+        std::string svalues;
+        int num_elements;
+        bool leaf;
+        std::string elements;
+    };
+}
+
+#endif //RECON_DBSTRUCT_H
---
+++ recon_daemon/DB_info.h
@@ -0,0 +1,12 @@
+#ifndef RECON_DB_INFO_H
+#define RECON_DB_INFO_H
+
+using namespace std;
+namespace DB_info{
+    const string host = "172.18.0.2";
+    const string database = "gpg_keyserver";
+    const string user = "root";
+    const string password = "";
+}
+
+#endif //RECON_DB_INFO_H
