51f2430ebfb3cb1f8b457d6943147a510ac8398b
Andrea Grazioso <grazioandre@gmail.com>
2018-07-22 20:35:33 +0200

    Added main class

---
+++ recon_daemon/main.cc
@@ -0,0 +1,40 @@
+#include "peer.h"
+#include "pTreeDB.h" 
+
+using namespace NTL;
+
+int main(int argc, char* argv[]){
+
+    std::cout << "Starting recon_daemon" << std::endl;
+
+    const char *MINUS_B = "-b";
+    const char *MINUS_R = "-r";
+    const char *MINUS_H = "-h";
+    ZZ_p::init(conv<ZZ>(Recon_settings::P_SKS_STRING.c_str()));
+
+    for (int i = 1; i < 2; i++){
+        if(!strcmp(argv[i], MINUS_H)){
+            std::cout << "Parameters:" << std::endl;
+            std::cout << "-b: build prefix tree" << std::endl;
+            std::cout << "-r: start recon" << std::endl;
+        }
+        else if(!strcmp(argv[i], MINUS_B)){
+            const Vec<ZZ_p> points = Utils::Zpoints(num_samples);
+            std::shared_ptr<RECON_DBManager> dbm = std::make_shared<RECON_DBManager>(); 
+            std::vector<std::string> hashes = dbm->get_all_hash();
+            Ptree tree(dbm, points);
+            tree.create();
+            tree.populate(hashes);
+            std::cout << "Inserted " << hashes.size() << " entry!" << std::endl; 
+        }
+        else if(!strcmp(argv[i], MINUS_R)){
+            const Vec<ZZ_p> points = Utils::Zpoints(num_samples);
+            std::shared_ptr<RECON_DBManager> dbm = std::make_shared<RECON_DBManager>(); 
+            Ptree tree(dbm, points);
+            tree.create();
+            Peer peer = Peer(tree);
+            peer.start();
+        }
+    }
+    exit(0);
+}
