391fe0149bc704d071d2aeb3367b672f7a20a521
Andrea Grazioso <grazioandre@gmail.com>
2018-08-27 13:42:45 +0200

    Delete ocaml recon daemon sources

--- recon_daemon_ml/pstyle.mli
+++
@@ -1,3 +0,0 @@
-val range : ?stride:int -> ?start:int -> int -> int list
-val ( </> ) : string -> int * int -> string
-val ( <|> ) : 'a array -> int * int -> 'a array
--- recon_daemon_ml/sks.mli
+++
@@ -1,1 +0,0 @@
-(* This is the base sks command *)
