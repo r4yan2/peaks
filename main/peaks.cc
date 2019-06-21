7bcceacdce62a8fc197ec79422fd0ea21a0a9320
Andrea Grazioso <grazioandre@gmail.com>
2019-06-21 20:43:36 +0200

    Adding tests

--- main/peaks.cc
+++ main/peaks.cc
@@ -1,7 +1,3 @@
-#include <fstream>
-#include <string>
-#include <iostream>
-#include <boost/program_options.hpp>
 #include <boost/exception/diagnostic_information.hpp>
 #include <exception>
 #include <syslog.h>
@@ -9,28 +5,13 @@
 #include <thread>
 #include <csignal>
 
+#include "peaks.h"
 #include "../cgi_handler/pks.h"
 #include "../recon_daemon/recon_daemon.h"
 #include "../unpacker/unpacker.h"
 #include "../import/import.h"
 #include "../analyzer/analyzer.h"
 #include "../dump/dump.h"
-
-/** convenient renaming for program_options, totally optional */
-namespace po = boost::program_options;
-
-/** help function shows up the help message when command line is incorrect */
-void help();
-
-/** function to parse config file
- * @param filename string which hold the name of the config file
- * @param vm variables_map of boost::program_options, because command line by default overrides config file
- */
-void parse_config(std::string filename, po::variables_map &vm);
-
-/** signal handler when SIGINT or SIGTERM are catched
- */
-void signalHandler( int signum );
 
 
 /** \mainpage Peaks Keyserver Documentation
@@ -42,8 +23,14 @@ void signalHandler( int signum );
  * easy to deploy and mantain and with a low resource footprint.
  *
  */
-        
+ 
+/** signal handler when SIGINT or SIGTERM are catched
+ */
 bool quitting = false;
+void signalHandler(int signum) {
+
+    quitting = true;
+}
 
 int main(int argc, char* argv[]){
 
@@ -202,133 +189,4 @@ int main(int argc, char* argv[]){
     }
 }
 
-void help(){
 
-    std::cout << "Usage: peaks [OPTIONS] COMMAND [ARGS]" << std::endl;
-
-    std::cout << std::endl;
-
-    std::cout << "Options:" << std::endl;
-    std::cout << "  -h, --help \t\tPrint this help message" << std::endl;
-    std::cout << "  -d, --debug \t\tTurn on debug output" << std::endl;
-    std::cout << "  -c, --config \t\tPath to the config file (If not provided it searches in the folder from which the executable is run)" << std::endl;
-
-    std::cout << "Commands and args:" << std::endl;
-    std::cout << std::endl;
-
-    std::cout << "  serve \t\tStart the webserver process" << std::endl;
-    std::cout << "    -c, --config \tspecify config file for cppcms" << std::endl;
-    std::cout << std::endl;
-
-    std::cout << "  build \t\tBuild the prefix-tree" << std::endl;
-
-    std::cout << std::endl;
-
-    std::cout << "  import \t\tImport certificates into Mysql" << std::endl;
-    std::cout << "    --threads \tSet number of threads to use" << std::endl;
-    std::cout << "    --keys \t\tSet how many keys a thread has to analyze" << std::endl;
-    std::cout << "    --path \t\tSet the path of the dump" << std::endl;
-    std::cout << "    --csv-only \t\tonly create temporary csv file, do not import into DB" << std::endl;
-    std::cout << "    --import-only \tonly import temporary csv, do not create anything" << std::endl;
-    std::cout << "    --fastimport \tDo not unpack certificates" << std::endl;
-    std::cout << "    --noclean \t\tdo not clean temporary folder" << std::endl;
-
-    std::cout << std::endl;
-
-    std::cout << "  dump \t\tDump database into csv files, currently output in MySQL directory" << std::endl;
-    std::cout << "    --threads \tSet number of threads to use" << std::endl;
-
-    std::cout << std::endl;
-
-    std::cout << "  unpack \t\tUnpack certificate not analyzer during fastimport" << std::endl;
-    std::cout << "    --threads \tSet number of threads to use" << std::endl;
-    std::cout << "    --keys \t\tSet how many keys a thread has to analyze" << std::endl;
-    std::cout << "    --limit \tSet the limit on key to unpack" << std::endl;
-    std::cout << "    --recover \tRecover previous broken session only" << std::endl;
-
-    std::cout << std::endl;
-
-    std::cout << "  analyze \t\tPerform security analysis on imported pubkeys" << std::endl;
-    std::cout << "    --threads \tSet number of threads to use" << std::endl;
-    std::cout << "    --keys \t\tSet how many keys a thread has to analyze" << std::endl;
-    std::cout << "    --limit \tSet the limit on key to analyze" << std::endl;
-
-    std::cout << std::endl;
-
-    std::cout << "  recon \t\tStart the recon process" << std::endl;
-    std::cout << "    --client-only \tStart only as client" << std::endl;
-    std::cout << "    --server-only \tStart only as server" << std::endl;
-    std::cout << "    --dryrun \t\tRecon without inserting into DB" << std::endl;
-
-    std::cout << std::endl;
-    exit(0);
-}
-
-void parse_config(std::string filename, po::variables_map &vm){
-    std::cout << "searching config file " << filename << std::endl;
-    std::ifstream cFile (filename);
-    if (cFile.is_open())
-    {
-        std::cout << "config file found!" << std::endl;
-        po::options_description config("Configuration");
-        config.add_options()
-            ("mbar", po::value<int>())
-            ("bq", po::value<int>())
-            ("max_ptree_nodes", po::value<int>())
-            ("ptree_thresh_mult", po::value<int>())
-            ("P_SKS_STRING", po::value<std::string>())
-            ("reconciliation_timeout", po::value<int>())
-            ("peaks_version", po::value<std::string>())
-            ("peaks_recon_port", po::value<int>())
-            ("peaks_http_port", po::value<int>())
-            ("peaks_filters", po::value<std::string>())
-            ("name", po::value<std::string>())
-            ("gossip_interval", po::value<int>())
-            ("max_read_len_shift", po::value<int>())
-            ("max_recover_size", po::value<int>())
-            ("default_timeout", po::value<int>())
-            ("max_request_queue_len", po::value<int>())
-            ("request_chunk_size", po::value<int>())
-            ("max_outstanding_recon_req", po::value<int>())
-            ("sks_bitstring", po::value<int>())
-            ("async_timeout_sec", po::value<int>())
-            ("async_timeout_usec", po::value<int>())
-            ("ignore_known_bug", po::value<int>())
-            ("unpack_on_import", po::value<int>())
-            ("max_unpacker_limit", po::value<unsigned int>())
-
-            ("db_user", po::value<std::string>())
-            ("db_host", po::value<std::string>())
-            ("db_database", po::value<std::string>())
-            ("db_password", po::value<std::string>()->default_value(""))
-            ("membership_config", po::value<std::string>())
-            ("cppcms_config", po::value<std::string>())
-            ("default_dump_path", po::value<std::string>())
-            ("analyzer_tmp_folder", po::value<std::string>())
-            ("analyzer_error_folder", po::value<std::string>())
-            ("analyzer_gcd_folder", po::value<std::string>())
-            ("unpacker_tmp_folder", po::value<std::string>())
-            ("unpacker_error_folder", po::value<std::string>())
-            ("recon_tmp_folder", po::value<std::string>())
-            ("import_tmp_folder", po::value<std::string>())
-            ("import_error_folder", po::value<std::string>())
-            ;
-        po::store(po::parse_config_file(cFile, config, false), vm);
-        vm.insert(std::make_pair("sks_zp_bytes", po::variable_value(17, false)));
-        vm.insert(std::make_pair("hashquery_len", po::variable_value(16, false)));
-        vm.insert(std::make_pair("num_samples", po::variable_value(vm["mbar"].as<int>() + 1, false)));
-        vm.insert(std::make_pair("split_threshold", po::variable_value(vm["ptree_thresh_mult"].as<int>() * vm["mbar"].as<int>(), false)));
-        vm.insert(std::make_pair("join_threshold", po::variable_value(vm["split_threshold"].as<int>() / 2, false)));
-        vm.insert(std::make_pair("max_read_len", po::variable_value(1 << vm["max_read_len_shift"].as<int>(), false)));
-
-    }
-    else {
-        throw std::runtime_error("Couldn't open config file for reading");
-    }
-}
-
-
-void signalHandler(int signum) {
-
-    quitting = true;
-}
--- test/pTree.cc
+++ test/pTree.cc
@@ -3,95 +3,19 @@
 #include <boost/program_options.hpp>
 #include <NTL/ZZ_p.h>
 #include "../recon_daemon/pTreeDB.h"
+#include "../main/peaks.h"
 
-namespace po = boost::program_options;
 using namespace boost::unit_test;
 
-void parse_config(std::string filename, po::variables_map &vm){
-    std::ifstream cFile (filename);
-    if (cFile.is_open())
-    {
-        po::options_description config("Configuration");
-        config.add_options()
-            ("mbar", po::value<int>())
-            ("bq", po::value<int>())
-            ("max_ptree_nodes", po::value<int>())
-            ("ptree_thresh_mult", po::value<int>())
-            ("P_SKS_STRING", po::value<std::string>())
-            ("reconciliation_timeout", po::value<int>())
-            ("peaks_version", po::value<std::string>())
-            ("peaks_recon_port", po::value<int>())
-            ("peaks_http_port", po::value<int>())
-            ("peaks_filters", po::value<std::string>())
-            ("name", po::value<std::string>())
-            ("gossip_interval", po::value<int>())
-            ("max_read_len_shift", po::value<int>())
-            ("max_recover_size", po::value<int>())
-            ("default_timeout", po::value<int>())
-            ("max_request_queue_len", po::value<int>())
-            ("request_chunk_size", po::value<int>())
-            ("max_outstanding_recon_req", po::value<int>())
-            ("sks_bitstring", po::value<int>())
-            ("async_timeout_sec", po::value<int>())
-            ("async_timeout_usec", po::value<int>())
-            ("ignore_known_bug", po::value<int>())
-            ("unpack_on_import", po::value<int>())
-            ("max_unpacker_limit", po::value<int>())
+struct test_global_fixture{
 
-            ("db_user", po::value<std::string>())
-            ("db_host", po::value<std::string>())
-            ("db_database", po::value<std::string>())
-            ("db_password", po::value<std::string>()->default_value(""))
-            ("membership_config", po::value<std::string>())
-            ("cppcms_config", po::value<std::string>())
-            ("default_dump_path", po::value<std::string>())
-            ("analyzer_tmp_folder", po::value<std::string>())
-            ("analyzer_error_folder", po::value<std::string>())
-            ("analyzer_gcd_folder", po::value<std::string>())
-            ("unpacker_tmp_folder", po::value<std::string>())
-            ("unpacker_error_folder", po::value<std::string>())
-            ("recon_tmp_folder", po::value<std::string>())
-            ("dumpimport_tmp_folder", po::value<std::string>())
-            ("dumpimport_error_folder", po::value<std::string>())
-            ;
-        po::store(po::parse_config_file(cFile, config, false), vm);
-        vm.insert(std::make_pair("sks_zp_bytes", po::variable_value(17, false)));
-        vm.insert(std::make_pair("hashquery_len", po::variable_value(16, false)));
-        vm.insert(std::make_pair("num_samples", po::variable_value(vm["mbar"].as<int>() + 1, false)));
-        vm.insert(std::make_pair("split_threshold", po::variable_value(vm["ptree_thresh_mult"].as<int>() * vm["mbar"].as<int>(), false)));
-        vm.insert(std::make_pair("join_threshold", po::variable_value(vm["split_threshold"].as<int>() / 2, false)));
-        vm.insert(std::make_pair("max_read_len", po::variable_value(1 << vm["max_read_len_shift"].as<int>(), false)));
-    }
-    else {
-        throw std::runtime_error("Couldn't open config file for reading");
-    }
-}
-
-struct db_fixture{
-    db_fixture(){
-    }
-
-    ~db_fixture(){}
-    static std::shared_ptr<Recon_memory_DBManager> dbm = std::make_shared<Recon_memory_DBManager>(db_settings);
-}
-
-struct ptree_fixture{
-    ptree_fixture(){
+    test_global_fixture(){
     
-    BOOST_TEST_MESSAGE( "setup fixture" );
+    BOOST_TEST_MESSAGE( "setup global fixture" );
     po::variables_map vm;
     parse_config( "./peaks_config", vm );
 
-    Recon_DBConfig db_settings = {
-        vm["db_host"].as<std::string>(),
-        vm["db_user"].as<std::string>(),
-        vm["db_password"].as<std::string>(),
-        vm["db_database"].as<std::string>(),
-        vm["recon_tmp_folder"].as<std::string>()
-    };
-
     NTL::ZZ_p::init(NTL::conv<NTL::ZZ>(vm["P_SKS_STRING"].as<std::string>().c_str()));
-    std::shared_ptr<Recon_memory_DBManager> dbm = std::make_shared<Recon_memory_DBManager>(db_settings);
     std::vector<NTL::ZZ_p> points = RECON_Utils::Zpoints(vm["num_samples"].as<int>());
 
     ptree_settings = {
@@ -106,45 +30,85 @@ struct ptree_fixture{
         vm["sks_bitstring"].as<int>()
     };
 
-    tree = Ptree(dbm, ptree_settings);
+    }
+
+    ~test_global_fixture(){}
+    
+    static Ptree_config ptree_settings;
+};
+
+Ptree_config test_global_fixture::ptree_settings = {};
+
+struct ptree_fixture{
+    ptree_fixture(){
+
+    std::shared_ptr<Recon_dummy_DBManager> dbm = std::make_shared<Recon_dummy_DBManager>();
+    tree = Ptree(dbm, test_global_fixture::ptree_settings);
     tree.create();
     }
 
     ~ptree_fixture(){
-        BOOST_TEST_MESSAGE( "teardown fixture" );
     }
 
     Ptree tree;
-    Ptree_config ptree_settings;
 };
 
-BOOST_TEST_GLOBAL_FIXTURE( ptree_fixture );
+BOOST_TEST_GLOBAL_FIXTURE( test_global_fixture );
 BOOST_FIXTURE_TEST_SUITE( ptree, ptree_fixture )
+
+BOOST_AUTO_TEST_CASE(ptree_test_create)
+{
+    BOOST_TEST_MESSAGE( "test create" );
+    // the root node should be empty
+    BOOST_CHECK_EQUAL( tree.get_node("")->get_num_elements() , 0 );
+    BOOST_CHECK_EQUAL( tree.get_root()->elements().size() , 0 );
+}
 
 BOOST_AUTO_TEST_CASE(ptree_test_insert)
 {
-    BOOST_TEST_MESSAGE( "test insert start" );
+    BOOST_TEST_MESSAGE( "test insert" );
     for (int i=100; i<1100; i++){
         NTL::ZZ_p num(i);
         tree.insert(num);
     }
     BOOST_CHECK_EQUAL( tree.get_node("")->get_num_elements() , 1000 );
-    BOOST_TEST_MESSAGE( "test insert end" );
+}
+
+BOOST_AUTO_TEST_CASE(ptree_test_insert_hash)
+{
+    BOOST_TEST_MESSAGE("test insert hash");
+    std::vector<std::string> test_vector = {
+        "000002D051097B858E1E04589B3200F5",
+        "0000030A302783B168C3E01C688AAC0D",
+        "0000031D1E9CBAE7FED390FDAC19B293",
+        "0000054BB0A05653D0684EBCD2799EBA",
+        "000007EDED8BAB4CF320621A56DA2F90",
+        "000009A4ECCDB5EA28EEDCA6BD7F8B0F",
+        "000009D84F42673DB71AB316C8D2D929",
+        "00000B9BA7B8296AEC0370715C813C30",
+        "00000C049313178A42FAA0FC62B40748",
+        "00000C2221E60B80EE54B23F80E582D2"
+    };
+    for (auto const& h: test_vector)
+        tree.insert(h);
+    std::vector<NTL::ZZ_p> elements_after_insert = tree.get_root()->elements();
+    BOOST_CHECK_EQUAL(elements_after_insert.size(), test_vector.size());
+
 }
 
 BOOST_AUTO_TEST_CASE(ptree_test_remove)
 {
-    BOOST_TEST_MESSAGE( "test remove start" );
+    BOOST_TEST_MESSAGE( "test remove" );
     for (int i=100; i<1100; i++){
         NTL::ZZ_p num(i);
         tree.insert(num);
     }
+    BOOST_REQUIRE_EQUAL( tree.get_node("")->get_num_elements(), 1000 );
     for (int i=100; i<1100; i++){
         NTL::ZZ_p num(i);
         tree.remove(num);
     }
     BOOST_CHECK_EQUAL( tree.get_node("")->get_num_elements() , 0 );
-    BOOST_TEST_MESSAGE( "test remove end" );
 }
 
 BOOST_AUTO_TEST_SUITE_END()
